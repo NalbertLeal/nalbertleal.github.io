0929e1d9c7d73b1af7fcf98fe96598913f98df48
Nalbert Gabriel Melo Leal <nalbertgml@gmail.com>
2020-08-29 19:11:13 -0300

    pomodoro PWA

---
+++ projects/pomodoro/service-worker.js
@@ -0,0 +1,32 @@
+const cacheName = 'pomodoro-nalbert-leal-v1'
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cachesNames => {
+        cachesNames.map(cache => {
+          if (cache !== cacheName) {
+            return caches.delete(cache)
+          }
+        })
+      })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.responWith(
+    fetch(event.request)
+      .then(res => {
+        const resClone = res.clone()
+        caches
+          .open(cacheName)
+          .then(cache => {
+            cache.put(event.request, resClone)
+          })
+        return res
+      })
+      .catch(err => caches.match(event.request)
+        .then(res => res))
+  )
+})
